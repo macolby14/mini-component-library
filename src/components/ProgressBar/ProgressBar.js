5652bbd40a917f6cb7d4d02dfa08ed005e129122
Mark Colby <macolby14@gmail.com>
2021-05-21 09:02:29 -1000

    create basic Progress Bar

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -5,8 +5,39 @@ import styled from 'styled-components';
 import { COLORS } from '../../constants';
 import VisuallyHidden from '../VisuallyHidden';
 
+const Wrapper = styled.div`
+  width: 370px;
+  height: var(--height);
+  background-color: ${COLORS.transparentGray15};
+  box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
+  border-radius: 8px;
+  padding: var(--padding);
+`;
+
+
+const Fill = styled.div`
+  background-color: ${COLORS.primary};
+  width: ${props => props.value}%;
+  height: 100%;
+  border-radius: ${({value}) => {
+    const interpolate = 4*Math.max((value-99),0); 
+    return `4px ${interpolate}px ${interpolate}px 4px`; 
+  }};
+`;
+
+const sizeStyles = {
+  small: {'--height': '8px', '--padding': 0},
+  medium: {'--height': '12px', '--padding': 0},
+  large: {'--height': '24px', '--padding': '4px'},
+};
+
 const ProgressBar = ({ value, size }) => {
-  return <strong>{value}</strong>;
+  value = Math.max(Math.min(100,value),0);
+
+  return <Wrapper style={sizeStyles[size]}>
+    <Fill value={value} />
+    <VisuallyHidden>{value}%</VisuallyHidden>
+    </Wrapper>;
 };
 
 export default ProgressBar;
